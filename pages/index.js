5378d26743b3eb09c9fea1c6834014de2dbf3402
steve <steve@meetfeli.com>
2018-04-25 18:29:53 +0200

    initial

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import {Component} from 'react';
+
+export default class index extends Component {
+    render(){
+        return (
+            <div>
+                <h1>hallosss</h1>
+            </div>
+        )
+    }
+}

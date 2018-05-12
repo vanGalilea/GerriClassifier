509d99fa6b99c321ced6a4c8a1a5ea45311260eb
steve <steve@meetfeli.com>
2018-05-12 22:46:33 +0200

    getting pixels and preparing train call pixelsArr is to long and gets the computer stuck

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,62 @@
 import {Component} from 'react';
+import brain from 'brain.js';
+
+const net = new brain.NeuralNetwork();
+
+var output = net.run({ r: 1, g: 0.4, b: 0 });
 
 export default class index extends Component {
+    componentDidMount() {
+        this.prepareData(document)
+    }
+
+    prepareData(document) {
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        const img = document.getElementById('myimg');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        context.drawImage(img, 0, 0 );
+        const imgData = context.getImageData(0, 0, img.width, img.height);
+        let pixelsArr = [];
+
+        for (let i = 0; i < imgData.data.length; i += 4) {
+            const pixel = {
+                r: imgData.data[i]/255,
+                g: imgData.data[i+1]/255,
+                b: imgData.data[i+2]/255
+            };
+
+            pixelsArr.push(pixel);
+        }
+
+        console.log(pixelsArr)
+    }
+
+    //
+    // trainModel(pixelsArr, isGerri) {
+    //     net.train([{input: pixelsArr, output: {isGerri}}]);
+    // }
+
+
+
+
+
     render(){
         return (
-            <div>
-                <h1>hallosss</h1>
+            <div id="hiddenPhotos">
+                <img id="myimg" src="/static/gerri.jpg" alt=""/>
+
+
+
+                <style jsx>
+                    {`
+                        img {
+                            display: none;
+                        }
+
+                    `}
+                </style>
             </div>
         )
     }
